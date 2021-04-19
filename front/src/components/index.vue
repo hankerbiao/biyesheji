c1068ac42ef1b4e85426ea187c006fd8b8ddd51c
pyhanker <3300434146@qq.com>
2021-04-19 15:10:15 +0800

    419

--- front/src/components/index.vue
+++ front/src/components/index.vue
@@ -3,11 +3,15 @@
     <el-backtop></el-backtop>
     <el-container>
       <div class="backimage">
-        <div class="header-title" style="margin-top: 20px; color: lightseagreen; line-height: 30px">
+        <div
+          class="header-title"
+          style="margin-top: 20px; color: lightseagreen; line-height: 30px"
+        >
           <el-row :gutter="20">
             <el-col :span="8">
               <p>
-                <i class="el-icon-star-off"></i>&nbsp;&nbsp;我的公众号:&nbsp;&nbsp;城建417
+                <i class="el-icon-star-off"></i
+                >&nbsp;&nbsp;我的公众号:&nbsp;&nbsp;城建417
               </p>
             </el-col>
             <el-col :span="16">
@@ -22,100 +26,103 @@
           <el-row type="flex" class="row-bg" justify="space-between">
             <el-col :span="4"> </el-col>
             <el-col :span="16">
-              <el-input placeholder="请输入关键字信息" v-model="imagesearch" class="input-with-select" style="font-size: 16px"
-                clearable></el-input>
+              <el-input
+                placeholder="请输入关键字信息"
+                v-model="imagesearch"
+                class="input-with-select"
+                style="font-size: 16px"
+                clearable
+              ></el-input>
             </el-col>
             <el-col :span="4">
-              <el-button type="primary" @click="searchcontent()" style="margin-left: -100px">搜 索</el-button>
+              <el-button
+                type="primary"
+                @click="searchcontent()"
+                style="margin-left: -100px"
+                >搜 索</el-button
+              >
             </el-col>
           </el-row>
         </div>
-        <div class="main-image">
-          <div class="row">
-            <div class="col-md-4 col-sm-6">
-              <div class="product">
-                <div class="product-img-wrap">
-                  <img class="card-img-top" src="../assets/images/600x400.png" alt="img" />
-                  <div class="quick-view">文字描述</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <div class="product">
-                <div class="product-img-wrap">
-                  <img class="card-img-top" src="../assets/images/600x280.png" alt="img" />
-                  <div class="quick-view">文字描述</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <div class="product">
-                <div class="product-img-wrap">
-                  <img class="card-img-top" src="../assets/images/600x320.png" alt="img" />
-                  <div class="quick-view">文字描述</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <div class="product">
-                <div class="product-img-wrap">
-                  <img class="card-img-top" src="../assets/images/600x600.png" alt="img" />
-                  <div class="quick-view">文字描述</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <div class="product">
-                <div class="product-img-wrap">
-                  <img class="card-img-top" src="../assets/images/600x400.png" alt="img" />
-                  <div class="quick-view">文字描述</div>
-                </div>
-              </div>
-            </div>
-          </div>
+
+        <div class="demo-image__preview1">
+          <el-image
+            style="width: 100px; height: 100px"
+            :src="url"
+            :preview-src-list="srcList"
+          >
+          </el-image>
+        </div>
+        <div class="demo-image__preview2">
+          <el-image
+            style="width: 100px; height: 100px"
+            :src="url"
+            :preview-src-list="srcList"
+          >
+          </el-image>
+        </div>
+        <div class="demo-image__preview2">
+          <el-image
+            style="width: 100px; height: 100px"
+            :src="url"
+            :preview-src-list="srcList"
+          >
+          </el-image>
+        </div>
+        <div class="demo-image__preview2">
+          <el-image
+            style="width: 100px; height: 100px"
+            :src="url"
+            :preview-src-list="srcList"
+          >
+          </el-image>
         </div>
       </div>
-      <!-- </div>
-    </div> -->
     </el-container>
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
+import axios from "axios";
 
-  export default {
-    data() {
-      return {
-        vedioCanPlay: false,
-        fixStyle: "",
-        imagesearch: "",
-      };
-    },
+export default {
+  data() {
+    return {
+      vedioCanPlay: false,
+      fixStyle: "",
+      imagesearch: "",
+      url:
+        "",
+      srcList: [],
+    };
+  },
 
-    methods: {
-      searchcontent(){
-        console.log(this.imagesearch)
-        console.log("点击了按钮")
+  methods: {
+    searchcontent() {
+      console.log(this.imagesearch);
+      console.log("点击了按钮");
 
-        const path = 'http://localhost:5003/test';
-        axios.get(path)
+      const path = "http://localhost:5003/test";
+      axios
+        .get(path)
         .then((res) => {
-          this.msg = res.data;
-          console.log(this.msg)
+          this.url = res.data.image_url;
+          this.srcList.push(res.data.draw_url);
+          console.log(this.srcList)
         })
         .catch((error) => {
           console.error(error);
         });
-      },
 
     },
-
-  };
+  },
+};
 </script>
 
 <style>
-  /* .backvideo {
+
+
+/* .backvideo {
     position: absolute;
     top: 0;
     left: 0;
@@ -140,7 +147,7 @@
     position: absolute;
     background: rgba(0, 0, 0, 0.4);
   } */
-    /* .backimage {
+/* .backimage {
       background-image: url(../assets/video.png);
       background-position: center 0;
       background-repeat: no-repeat;
@@ -152,20 +159,20 @@
       -ms-background-size: cover;
     } */
 
-  .main-image {
-    /* background-color: #ffffff; */
+/* .main-image {
     float: left;
     margin: 20px;
-  }
+  } */
+/* 
+    .el-input {
+      width: 100%;
+    }
 
-  .el-input {
-    width: 100%;
-  }
+    .searchbar {
+      margin: 24px;
+    } */
 
-  .searchbar {
-    margin: 24px;
-  }
-
+/* 
   .product {
     transition: 0.4s;
     background: #ffffff;
@@ -208,11 +215,10 @@
     transition: 0.5s;
     height: 50px;
     font-weight: 600;
-    /* margin-top: 100px; */
     padding: 10px;
   }
 
   .product:hover .product-img-wrap .quick-view {
     opacity: 1;
-  }
+  } */
 </style>
